9a0415629b9e611e741a318b05f9d615d5f6bec6
Andra <65946647+andrapixel@users.noreply.github.com>
2023-05-25 06:33:53 +0300

    Explained #undefined lines of code in config.py

--- config.py
+++ config.py
@@ -18,72 +18,73 @@ import torch
 from torch.backends import cudnn
 
 # Random seed to maintain reproducible results
-random.seed(0) ##undefined
-torch.manual_seed(0) ##undefined
-np.random.seed(0) ##undefined
+random.seed(0) # set the random seed for reproducibility to 0
+torch.manual_seed(0) # set the random seed for torch operations to 0
+np.random.seed(0) # set the random seed for numpy operations to 0
+
 # Use GPU for training by default
-device = torch.device("cuda", 0) ##undefined
+device = torch.device("cuda", 0) # set the device for training to GPU (cuda:0)
 # Turning on when the image size does not change during training can speed up training
-cudnn.benchmark = True ##undefined
+cudnn.benchmark = True # Enable the cuDNN benchmark mode for faster training
 # Model arch name
-model_arch_name = "mobilenet_v1" ##undefined
+model_arch_name = "mobilenet_v1"  # set the name of the model architecture (MobileNetV1)
 # Model normalization parameters
-model_mean_parameters = [0.485, 0.456, 0.406] ##undefined
-model_std_parameters = [0.229, 0.224, 0.225] ##undefined
+model_mean_parameters = [0.485, 0.456, 0.406] # define the mean values for normalization of input images
+model_std_parameters = [0.229, 0.224, 0.225]  # define the standard deviation values for normalization of input images
+
 # Model number class
-model_num_classes = 1000 ##undefined
+model_num_classes = 1000 # set the number of classes in the model to 0
 # Current configuration parameter method
-mode = "train" ##undefined
+mode = "train" # set the current mode of the configuration to training mode
 # Experiment name, easy to save weights and log files
-exp_name = f"{model_arch_name}-ImageNet_1K" ##undefined
+exp_name = f"{model_arch_name}-ImageNet_1K" # set the experiment name based on model architecture and dataset
 
 if mode == "train":
     # Dataset address
     train_image_dir = "./data/ImageNet_1K/ILSVRC2012_img_train"
     valid_image_dir = "./data/ImageNet_1K/ILSVRC2012_img_val"
 
-    image_size = 224 ##undefined
-    batch_size = 128 ##undefined
-    num_workers = 4 ##undefined
+    image_size = 224 # set the size of the input images (224x224)
+    batch_size = 128 # set the batch size for training to 128
+    num_workers = 4 # define the number of workers for data loading (4)
 
     # The address to load the pretrained model
-    pretrained_model_weights_path = "" ##undefined
+    pretrained_model_weights_path = "" # path to load pre-trained model weights (initially empty for no pre-training)
 
     # Incremental training and migration training
-    resume = "" ##undefined
+    resume = "" # path to resume training from a saved checkpoint (empty for starting from scratch)
 
     # Total num epochs
-    epochs = 600 ##undefined
+    epochs = 600 # set the total number of training epochs to 600
 
     # Loss parameters
-    loss_label_smoothing = 0.1 ##undefined
-    loss_weights = 1.0 ##undefined
+    loss_label_smoothing = 0.1 # set the label smoothing factor for the loss function to 0.1
+    loss_weights = 1.0 # set the weight for the loss function to 1
 
     # Optimizer parameter
-    model_lr = 0.1 ##undefined
-    model_momentum = 0.9 ##undefined
-    model_weight_decay = 2e-05 ##undefined
-    model_ema_decay = 0.99998 ##undefined
+    model_lr = 0.1 # set the learning rate for the optimizer to 0.1
+    model_momentum = 0.9  # set the momentum factor for the optimizer to 0.9
+    model_weight_decay = 2e-05  # set the weight decay factor for the optimizer
+    model_ema_decay = 0.99998  # set the exponential moving average decay factor for the optimizer
 
     # Learning rate scheduler parameter
-    lr_scheduler_T_0 = epochs // 4 ##undefined
-    lr_scheduler_T_mult = 1 ##undefined
-    lr_scheduler_eta_min = 5e-5 ##undefined
+    lr_scheduler_T_0 = epochs // 4  # define the number of iterations to decrease learning rate (T_0)
+    lr_scheduler_T_mult = 1 # multiply T_0 by this factor after each restart (T_mult)
+    lr_scheduler_eta_min = 5e-5 # set the minimum learning rate (eta_min) for the scheduler
 
     # How many iterations to print the training/validate result
-    train_print_frequency = 200 ##undefined
-    valid_print_frequency = 20 ##undefined
+    train_print_frequency = 200 # set the frequency of printing training results to 200
+    valid_print_frequency = 20 # set the frequency of printing validation results to 20
 
-if mode == "test": ##undefined
+if mode == "test": # if the model enters the testing mode
     # Test data address
-    test_image_dir = "./data/ImageNet_1K/ILSVRC2012_img_val" ##undefined
+    test_image_dir = "./data/ImageNet_1K/ILSVRC2012_img_val"  # get the directory path for test dataset
 
     # Test dataloader parameters
-    image_size = 224 ##undefined
-    batch_size = 256 ##undefined
-    num_workers = 4 ##undefined
+    image_size = 224 # set the size of the input images (224x224)
+    batch_size = 256 # set the batch size for testing to 256
+    num_workers = 4 # define the number of workers for data loading
 
     # How many iterations to print the testing result
-    test_print_frequency = 20 ##undefined
-
-    model_weights_path = "results/pretrained_models/Xception-ImageNet_1K-a0b40234.pth.tar" ##undefined
+    test_print_frequency = 20 # define the frequency of printing test results and set it to 20
+    model_weights_path = "results/pretrained_models/Xception-ImageNet_1K-a0b40234.pth.tar" # get the path to the pretrained model weights file
